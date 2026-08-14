fb8ea6c35646b9a692c5dc20a7d3553c2b5e7695
japanesesakura301-sketch <japanesesakura301@gmail.com>
2026-08-14 23:29:31 +0545

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// १. Supabase Project details (तपाईंको आफ्नै Credentials हाल्नुहोस्)
+const SUPABASE_URL = "sb_publishable_OklNinQoIp8pNNXcUKThvw_WfvHD9ws";
+const SUPABASE_ANON_KEY = "sb_secret__0sCim2Q5LsMfT3ZXY2oOQ_0XQ2I-yS";
+
+// २. Supabase Client Initialize गर्ने
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+ 
